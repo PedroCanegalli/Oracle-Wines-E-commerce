4514b4039c6cbd76b953c4aec7176ff418087669
PedroCanegalli <104397497+PedroCanegalli@users.noreply.github.com>
2022-06-27 18:16:34 -0300

    Delete administrador.js

--- administrador.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-let administradorRouter = require("../controller/homeController")
-
-router.get("/", administradorRouter.administrador)
-
-
-module.exports = router;
