9c69634b18acc312eaa7b0dd21ca9d939e249ad7
francmauric <franc_mauric1@hotmail.com>
2022-07-16 10:17:50 -0300

    proceso de edit terminado

--- app.js
+++ app.js
@@ -38,35 +38,6 @@ app.use('/administrador', administradorRouter);
 app.use('/edit-product', editProduct)
 
 
-/*
-app.get("/", (req,res)=> {
-    res.render(path.resolve(__dirname,"./views/home.ejs"))
-})
-
-app.get("/registro", (req,res)=> {
-    res.render(path.resolve(__dirname,"./views/registro.ejs"))
-})
-
-app.get("/carrito", (req,res)=> {
-    res.render(path.resolve(__dirname,"./views/carrito.ejs"))
-})
-
-app.get("/producto", (req,res)=> {
-    res.render(path.resolve(__dirname,"./views/productDetail.ejs"))
-})
-
-app.get("/login", (req,res)=> {
-    res.render(path.resolve(__dirname,"./views/login.ejs"))
-})
-
-app.get("/recuperar", (req,res)=> {
-    res.render(path.resolve(__dirname,"./views/recuperar.ejs"))
-})
-
-app.get("/historial", (req,res)=> {
-    res.render(path.resolve(__dirname,"./views/historial.ejs"))
-})
-*/
 app.listen(3100,() => {
     console.log("wey esta madre si funciona")
 })
